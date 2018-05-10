9698b9d0fa4f54c6af16108a2e4995234f5c75c7
lvchengjie <lvchengjie@iyunxiao.com>
2018-05-10 15:21:14 +0800

    add showTitle

--- express-boilerplate_find/routes/boke.js
+++ express-boilerplate_find/routes/boke.js
@@ -157,4 +157,72 @@ router.post('/addtopical', function (req, res){
 });
 
 
+//显示标题
+router.get('/showTitle', function (req, res){
+
+    let errMsg = utils.buildErrMsg(req, apis.ADDTOP);
+    
+     //分页信息
+     let limit;
+     let currentPage;
+     if(req.body['limit']){
+         limit = req.body['limit'];
+     }else{
+         limit = 10;
+     }
+     if(req.body['currentPage']){
+         //加上合法判断    
+         currentPage = req.body['currentPage'];
+     }else{
+         currentPage = 1;
+     }
+
+    Thenjs(function(cont){
+        boke.count( function(err, count){
+            if(err){
+                return cont(err);
+            }
+            if(count === 0){
+                errMsg.msg = '没有数据';
+                let jsonMSG = utils.resJSON(errCodes.SHOWPAPER_ERR, errMsg.msg, null, errMsg);
+                return res.json(jsonMSG);
+            }
+            return cont(null, count);
+        });
+    }).then(function(cont, count){
+        //获取总页数
+        let totalpage = Math.floor(count / limit);
+        //如果最后一页不到limit行数，总页数+1
+        if (count % limit !== 0) {
+            totalpage += 1;
+        }
+        //如果当前页比最大页数大，则转到最后一页
+        if (currentPage > totalpage) {
+            currentPage = totalpage;
+        }
+        //进行分页 
+        let skips = (currentPage - 1) * limit;
+        let show_ct = {'article.title': 1, 'article.date' : 1};
+        boke.find().sort({'article.date' : -1}).project(show_ct).skip(skips).limit(limit).toArray(function(err, result){
+            if(err){
+                return cont(err);
+            }
+            if(result.length === 0){
+                errMsg.msg = '没有数据';
+                let jsonMSG = utils.resJSON(errCodes.SHOWTITLE_ERR, errMsg.msg, null, errMsg);
+                return res.json(jsonMSG);
+            }
+            let results = [];
+            for(let i = 0; i < result.length; i++){
+                results[i] = {'title' : result[i].article.title, 'date' : result[i].article.date};
+            }
+            errMsg.msg = 'success';
+            return res.json(utils.resJSON(errCodes.SUCCESS, errMsg.msg, results, errMsg));  
+        });
+    }).fail(function(cont, err){
+        errMsg.msg = '数据库错误';
+        return res.json(utils.resJSON(errCodes.DB_ERR, errMsg.msg, null, errMsg));
+    }); 
+});
+
 module.exports = router;
